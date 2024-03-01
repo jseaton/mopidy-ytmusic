e5340ffd1ad6e6f556d910d7d4f80a876a9a4247
Jo Seaton <js-git@sigmanetwork.org>
2024-03-01 13:25:24 +0000

    Bump ytmusicapi and pytube dependencies to more recent

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ package_data = {"": ["*"]}
 
 install_requires = [
     "Mopidy>=3,<4",
-    "pytube>=12.1.0",
-    "ytmusicapi>=0.22.0,<2.0.0",
+    "pytube==15.0.0",
+    "ytmusicapi>=1.5.4",
 ]
 
 entry_points = {"mopidy.ext": ["ytmusic = mopidy_ytmusic:Extension"]}
